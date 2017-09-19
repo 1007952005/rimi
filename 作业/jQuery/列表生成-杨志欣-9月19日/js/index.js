02c6ffadc6144ff8d7b9ab6d3c06eabbf30102bf
Pudge <30584286+1007952005@users.noreply.github.com>
2017-09-19 19:56:24 +0800

    列表生成

---
+++ 作业/jQuery/列表生成-杨志欣-9月19日/js/index.js
@@ -0,0 +1,44 @@
+window.onload = function () {
+    var $sectionBox = $(".sectionBox"),
+        $article = $("article");
+    //显示列表
+    $("#showList").click(function () {
+        $sectionBox.fadeToggle(300);
+        //恢复默认值
+        $("input").val("");
+        $("textarea").val("");
+    });
+    //添加列表事件
+    $("#confirm").click(function () {
+        var titleText = $("input").val(),
+            textContent = $("textarea").val();
+        //列表内容不能为空
+        if (titleText == "" || textContent == "") {
+            return;
+        }
+        var str = `<section>
+            <h2>${titleText}</h2>
+            <p>${textContent}</p>
+            <span class="close"></span>
+        </section>`;
+        //添加列表
+        $article.append(str);
+        //绑定关闭事件
+        $(".close:last").click(function (event) {
+            $(this).parent().fadeOut(200, function () {
+                this.remove();
+            })
+            //阻止事件冒泡防止列表展开事件触发
+            event.stopPropagation();
+        });
+        //列表展开事件
+        $("section:last").click(function () {
+            console.log($(this));
+            $(this).siblings().find("p").slideUp(300);
+            $(this).find("p").slideToggle(300);
+            $(this).css("background-color", "#e8e8e8");
+        });
+        //关闭列表
+        $sectionBox.fadeOut(300);
+    });
+}
